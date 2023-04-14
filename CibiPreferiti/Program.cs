c77457b25b3c2ba18f942769b629771a07477d90
UMBY2407 <umby2407@gmail.com>
2023-04-14 15:10:16 +0200

    Added my list of 10 favorite food; my top and Least favorite food

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿	//MAIN PROGRAM
+
+string[] favoriteFood = {"Pizza Napoletana", "Fassona", "Vitel Tonnè", "Pesce Spada", "Bonet", "Tiramisù", "Patatine Fritte", "Fritto Misto", "Pizza Fritta", "Carbonara"};
+ShowNumberOfFood(favoriteFood);
+ShowGradedList(favoriteFood);
+PrintTopFood(favoriteFood);
+PrintLeastFood(favoriteFood);
+
+	//FUNCTIONS
+
+void ShowNumberOfFood(string[] array)
+{
+	Console.WriteLine("La mia classifica dei cibi preferiti contiene: " + array.Length + " elementi");
+}
+
+void ShowGradedList(string[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.WriteLine($"{i+1}° {array[i]}");
+	}
+}
+
+void PrintTopFood(string[] array)
+{
+	for (int i = 0;i < array.Length; i++)
+	{
+		if(i == 0)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("Come potete vedere, il mio piatto preferito è la: " + array[i]);
+		}
+	}
+}
+
+void PrintLeastFood(string[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (i == array.Length - 1)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("Mentre l'ultimo dei miei piatti preferiti è la: " + array[i]);
+		}
+	}
+}
