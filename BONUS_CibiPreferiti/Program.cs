c65174e8885cc49ca8cfc0ce0e80edd01e77d014
UMBY2407 <umby2407@gmail.com>
2023-04-14 16:10:35 +0200

    BONUS: Ask user his fav. food

--- BONUS_CibiPreferiti/Program.cs
+++ BONUS_CibiPreferiti/Program.cs
@@ -2,18 +2,17 @@
 
 string[] favoriteFood = {};
 favoriteFood = CreateListOfFood(favoriteFood);
-ShowGradedList(favoriteFood);
 
 //FUNCTIONS
 
 string[] CreateListOfFood(string[] array)
 {
-	Console.Write("Quanti piatti vorresti mettere nella lista? ");
+	Console.Write("Quanti piatti vuoi aggiungere alla tua lista? ");
 	int foodNumber = int.Parse(Console.ReadLine());
 	array = new string[foodNumber];
 	for (int i = 0; i < array.Length; i++)
 	{
-		Console.Write("Inserisci in ordine di gradimento il piatto: ");
+		Console.Write("Inserisci in ordine di preferenza il piatto: ");
 		array[i] = Console.ReadLine();
 	}
 	return array;
